1ee76f8f88cb6db6490f3bc91a2f7895649240f4
Abdul Ahad <ahadkhana182@gmail.com>
2024-08-29 14:16:53 +0500

    fourth

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,46 @@
-import { useEffect } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import CardDetail from './components/CardDetail';
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import CardDetail from './components/CardDetail'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { pdfjs } from 'react-pdf';
-import './App.css';
 
-// Set PDF.js worker source
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
   import.meta.url,
 ).toString();
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: 'project_detail/:id',
-          element: <CardDetail />,
-        },
-      ],
-    },
-  ]);
+ const router = createBrowserRouter([
 
-  useEffect(() => {
-    AOS.init();
-  }, []);
-
+  {
+    path:"/",
+    element:<Layout/>,
+    children:[
+      {
+        index: true,
+        element: <Home/>
+      },
+      {
+        path:'project_detail/:id',
+        element: <CardDetail/>
+      }
+    ]
+  }
+ ])
+useEffect(()=>{
+  AOS.init();
+},[])
   return (
     <>
-      <RouterProvider router={router} />
+    <RouterProvider router={router}/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
